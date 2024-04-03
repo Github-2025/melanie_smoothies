5ef1c20e8b49dd9bf053f2243e1935ecf5e4d734
Github-2025 <164141544+Github-2025@users.noreply.github.com>
2024-04-03 22:06:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -8,18 +9,12 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!"""
 )
 
-#option = st.selectbox('What is your favorite fruit?',
-#    ('Banana', 'Strawberries', 'Peaches'))
-#
-#st.write('Your favorite fruit is:', option)
-
 Name_On_Order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', Name_On_Order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -28,23 +23,15 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-#    st.write(ingredients_list)
-#    st.text(ingredients_list)
-
     ingredients_string = ''
-
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-#    st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, Name_On_Order)
             values ('""" + ingredients_string + """', '""" + Name_On_Order + """')"""
 
-#    st.write(my_insert_stmt)
-#    st.stop()
-    
-#    st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
@@ -52,7 +39,5 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered,' + Name_On_Order + '!', icon = "✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+
+
