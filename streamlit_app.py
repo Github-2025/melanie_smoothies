5e9fa5f0dd41a17999875abe860a9b89833360d7
Github-2025 <164141544+Github-2025@users.noreply.github.com>
2024-04-03 18:08:31 +0530

    Update streamlit_app.py
    Connected changes needed bc to move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write(
 Name_On_Order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', Name_On_Order)
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
